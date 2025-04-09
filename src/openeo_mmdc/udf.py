f99bf6ff6a3f337455993585495b5b8fa836754b
ekalinicheva <ek.kalinicheva@gmail.com>
2025-04-09 17:00:19 +0200

    Data processing functions added

--- src/openeo_mmdc/udf.py
+++ src/openeo_mmdc/udf.py
@@ -18,8 +18,8 @@ from openeo.udf import XarrayDataCube
 # NEW_BANDS = [f"F0{i}" for i in range(10)] + [f"F{i}" for i in range(10, 64)]
 NEW_BANDS = ["F01_mu", "F02_mu", "F03_mu", "F04_mu", "F05_mu", "F06_mu",
              "F01_logvar", "F02_logvar", "F03_logvar", "F04_logvar", "F05_logvar", "F06_logvar"]
-#
-#
+
+
 def apply_metadata(metadata: CollectionMetadata, context: dict) -> CollectionMetadata:
     """Apply metadata"""
     return metadata.rename_labels(
@@ -37,6 +37,7 @@ def check_datacube(cube: xr.DataArray):
         raise RuntimeError(
             "DataCube should have at least 3 days of temporal series)"
         )
+
 
 def s2_angles_processing(s2_angles_data: np.ndarray) -> np.ndarray:
     """
@@ -61,6 +62,30 @@ def s2_angles_processing(s2_angles_data: np.ndarray) -> np.ndarray:
         ],
         axis=1,
     )
+
+
+def s1_angles_processing(angles_s1_asc: np.array, angles_s1_desc: np.array) -> np.array:
+    """Transform S1 angles"""
+    angles = np.concat((angles_s1_asc, angles_s1_desc), 1)
+
+    processed_angles = np.zeros_like(angles)
+    processed_angles[(angles != np.nan) | (angles != 0)] = np.cos(
+        np.deg2rad(angles[(angles != np.nan) | (angles != 0)])
+    )
+
+    return processed_angles
+
+
+def apply_log_to_s1(data: np.array) -> np.array:
+    """
+    Apply log to S1 data
+    """
+    clip_min: float = 1e-4
+    clip_max: float = 2.0
+
+    nan_idxs = ~np.isnan(data)
+    data[nan_idxs] = np.log10(np.clip(data[nan_idxs], clip_min, clip_max))
+    return data
 
 
 def dem_height_aspect(dem_data: np.ndarray) -> np.ndarray:
@@ -91,7 +116,8 @@ def dem_height_aspect(dem_data: np.ndarray) -> np.ndarray:
 
     return np.stack([dem_data, sin_slope, cos_aspect, sin_aspect])
 
-def run_inference(input_data: np.ndarray, satellite) -> np.ndarray:
+
+def run_inference(input_data: np.ndarray, satellite: str = "s2") -> np.ndarray:
     """
     Inference function for Sentinel-2 embeddings with prosailVAE.
     The input should be in shape (B, C, H, W)
@@ -118,13 +144,12 @@ def run_inference(input_data: np.ndarray, satellite) -> np.ndarray:
     ro = ort.RunOptions()
     ro.add_run_config_entry("log_severity_level", "3")
 
-
-    if satellite.lower() == "s2":
+    if satellite.lower() == "s2" or input_data.shape[1] == 63:
         # We transform input data in right format
         s2_ref, s2_angles, meteo, dem = (
             input_data[:, :10].astype(np.float32),
             input_data[:, 10:14].astype(np.float32),
-            input_data[:, 14:14+48].astype(np.float32),
+            input_data[:, 14:14 + 48].astype(np.float32),
             input_data[:, -1].astype(np.float32)
         )
 
@@ -132,9 +157,25 @@ def run_inference(input_data: np.ndarray, satellite) -> np.ndarray:
         dem = np.stack([dem_height_aspect(d) for d in dem])
         angles = s2_angles_processing(s2_angles)
 
-    elif input_data.shape[1]==15:
-        s1_ref, s1_angles = input_data[:, :10].astype(np.float32), input_data[:, 10:].astype(np.float32)
-
+    elif input_data.shape[1] == 15:
+        s1_asc, s1_desc, s1_angles_asc, s1_angles_desc, meteo, dem = (
+            input_data[:, :2].astype(np.float32),
+            input_data[:, 2:3].astype(np.float32),
+            input_data[:, 3:5].astype(np.float32),
+            input_data[:, 5:6].astype(np.float32),
+            input_data[:, 6:6 + 48].astype(np.float32),
+            input_data[:, -1].astype(np.float32)
+        )
+        image = np.concat(
+            (
+                s1_asc, s1_asc[:, 1] / s1_asc[:, 0] + 3e-05,
+                s1_desc, s1_desc[:, 1] / s1_desc[:, 0] + 3e-05
+            ),
+            axis=1
+        )
+        image = apply_log_to_s1(image)
+        dem = np.stack([dem_height_aspect(d) for d in dem])
+        angles = s1_angles_processing(s1_angles_asc, s1_angles_desc)
     else:
         raise IndexError
 
